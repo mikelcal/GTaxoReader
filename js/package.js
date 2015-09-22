bdb7eaeec87948db370bfa8a28676101b7ce5a7e
Mikel Calderon <mikel.cal@gmail.com>
2015-09-21 21:22:28 -0700

    Updated install workflow
    install workflow to individual js file

---
+++ js/package.js
@@ -0,0 +1,33 @@
+/* Adapted from https://github.com/robnyman/Firefox-OS-Boilerplate-App/blob/gh-pages/js/base.js */
+/* global alert, console */
+"use strict";
+// Install app
+if (navigator.mozApps) {
+    var checkIfInstalled = navigator.mozApps.getSelf();
+    checkIfInstalled.onsuccess = function () {
+        if (checkIfInstalled.result) {
+            // Already installed
+            var installationInstructions = document.querySelector("#install-instructions");
+            if (installationInstructions) {
+                installationInstructions.style.display = "none";
+            }
+        }
+        else {
+            var install = document.querySelector("#install"),
+                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+            install.className = "start-install";
+            install.onclick = function () {
+                var installApp = navigator.mozApps.install(manifestURL);
+                installApp.onsuccess = function() {
+                    install.style.display = "none";
+                };
+                installApp.onerror = function() {
+                    alert("Install failed\n\n:" + installApp.error.name);
+                };
+            };
+        }
+    };
+}
+else {
+    console.log("Open Web Apps not supported");
+}
